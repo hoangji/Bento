bb1200fb843c5702e6c6f424ff06a560b5b20aee
hoangji <lukasnguyen@hotmail.com>
2023-02-10 01:14:25 +0100

    bento v1.2

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '37.77547.2692',
+	defaultLongitude: '11.4041',
 
 	// Autochange
 	autoChangeTheme: true,
